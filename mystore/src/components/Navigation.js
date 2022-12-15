a76c2e7545a9aea9289f4f202fbcf78e7ef11ade
Prashant Mishra <prashantmishra111@outlook.com>
2022-12-15 18:12:30 +0530

    !

--- mystore/src/components/Navigation.js
+++ mystore/src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Navigation.css"
- 
+
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Outlet, Link } from "react-router-dom";
 
@@ -8,24 +8,26 @@ import { Outlet, Link } from "react-router-dom";
 
 function Navigation({ size, setShow }) {
 
-   
+
 
   return (
     <div className='nav_container'>
-       
-      <h1  >myStore</h1>
+
+      <h1  ><i className="fa-solid fa-location-dot" />MYSTORE  </h1>
       <nav  >
         <ul className='navLinks' >
           <li  ><Link to="/">Home  </Link>   </li>
-          <li 
+          <li
           // onClick={() => setShow(true)}
           ><Link to="/shop">Shop </Link>         </li>
           <li>  Knowledge  </li>
           <li>  Track Order </li>
-          
+
         </ul>
-        <span onClick={() => setShow(false)}>
-        <Link to="/cart">   <i className="fas fa-cart-plus"></i></Link>
+        <span
+         id = "cart-logo"
+         onClick={() => setShow(false)}>
+          <Link to="/cart">   <i className="fas fa-cart-plus"></i></Link>
           <span>{size}</span>
         </span>
       </nav>
